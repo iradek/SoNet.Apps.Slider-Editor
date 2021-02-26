25cc53951e5c2fba7c818d43d5ddcff3419cdad2
Radek <radek@iradek.com>
2021-02-26 10:21:47 -0800

    Working Api calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,21 @@ declare var $: any;
 })
 export class AppComponent {
 
-    @ViewChild("editSlider") editSliderControl!: EditSlider;
-    @ViewChild("slideEditor") slideEditor!: ElementRef;
-    @ViewChildren(EditSliderItemComponent) editSliderItemControls!: QueryList<EditSliderItemComponent>;
-    @ViewChildren("accordionHeader") accordionHeaders?: QueryList<ElementRef>;
+    @ViewChild("editSlider", { static: true }) editSliderControl: EditSlider;
+    @ViewChild("slideEditor", { static: true }) slideEditor: ElementRef;
+    @ViewChildren(EditSliderItemComponent) editSliderItemControls: QueryList<EditSliderItemComponent>;
+    @ViewChildren("accordionHeader") accordionHeaders: QueryList<ElementRef>;
     maxUploadLengthInMegaBytes: number = 300;
 
     get currentSlider(): Slider {
-        return this.editSliderControl!.getSliderObject();
+        return this.editSliderControl.getSliderObject();
     }
     set currentSlider(value: Slider) {
         this.editSliderControl.slider = value;
     }
 
     get canSave(): boolean {
-        return this.editSliderControl!.valid;
+        return this.editSliderControl.valid;
     }
 
     constructor(private apiClient: SliderApiClient, private renderer: Renderer2, private messageService: MessageService) {
@@ -126,7 +126,7 @@ export class AppComponent {
                 newSliderItem.SlideIndicatorStyle = lastSliderItem.SlideIndicatorStyle;
         }
         this.currentSlider.SliderItemList.push(newSliderItem);
-        setTimeout(() => this.accordionHeaders!.last!.nativeElement!.click(), 100);//wait for rendering tick
+        setTimeout(() => this.accordionHeaders.last.nativeElement.click(), 100);//wait for rendering tick
     }
 
     trackSliderItem(index: number, sliderItem: SliderItem) {
